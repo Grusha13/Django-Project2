58983c5f4c34ca958888764638a11c2f0640d4f4
Grusha13 <90636234+Grusha13@users.noreply.github.com>
2022-05-20 00:32:56 +0530

    Add files via upload

---
+++ myworld/members/views.py
@@ -0,0 +1,28 @@
+from audioop import reverse
+from django.http import HttpResponse, HttpRequest, HttpResponseRedirect
+from django.template import loader
+from .models import Members
+
+def index(request):
+    mymembers = Members.objects.all().values()
+    template = loader.get_template('myfirst.html')
+    context = {
+        'mymembers': mymembers,
+    }
+    return HttpResponse(template.render(context, request))
+
+def add(request):
+    template = loader.get_template('add.html')
+    return HttpResponse(template.render({}, request))
+
+def addrecord(request):
+  x = request.POST['first']
+  y = request.POST['last']
+  member = Members(firstname=x, lastname=y)
+  member.save()
+  return HttpResponseRedirect(reverse('index'))
+
+def delete(request, id):
+  member = Members.objects.get(id=id)
+  member.delete()
+  return HttpResponseRedirect(reverse('index'))
